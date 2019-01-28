cbfee65577b58cf7475cf6b2383e9b19edf07125
Hukanzen <qazxswed1934@gmail.com>
2019-01-29 07:53:31 +0900

    ソースの読解

--- src/game/Move_backcard.java
+++ src/game/Move_backcard.java
@@ -12,13 +12,15 @@ public class Move_backcard extends JPanel implements Runnable {
 	BufferedImage backcard = null;
 	boolean flag;
 	public Move_backcard() {
+		super();
+		setLayout(null);
+		setVisible(true);
 	}
 	
 	public void Move_from_deck(BufferedImage tmpcard, int k) {
 		 backcard = tmpcard;
 		 x = check_place(k);
 		 flag = true;
-		 repaint();
 		 mv = new Thread(this);
 		 mv.start();
 	}
@@ -39,6 +41,7 @@ public class Move_backcard extends JPanel implements Runnable {
 		super.paintComponent(g);
 		System.out.printf("Move_backcard paintComponent: %d\n",  l);
 		g.drawImage(backcard, l,  y,  100,  150,  this);	
+		g.dispose();
 	}
 	
 	@Override
